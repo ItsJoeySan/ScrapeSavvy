eded75ad7c8b618f7933accc8c7dfef1d072d39b
ItsJoey <ItsJoeySan@gmail.com>
2024-11-25 19:51:27 +0530

    a checked added for existing product

--- lib/actions/createProduct.ts
+++ lib/actions/createProduct.ts
@@ -3,6 +3,12 @@
 import prisma from "../prismaClient/prisma";
 
 const createProduct = async (data: any) => {
+  const existingProduct = await prisma.product.findFirst({
+    where: { title: data.title },
+  });
+  if (existingProduct) {
+    return existingProduct;
+  }
   const product = await prisma.product.create({ data });
   return product;
 };
