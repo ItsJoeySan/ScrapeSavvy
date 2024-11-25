9cf90c19ff6eb79ad0acee4ee214c810621c9046
ItsJoey <ItsJoeySan@gmail.com>
2024-11-25 20:41:15 +0530

    checking if product exists

--- lib/actions/createProduct.ts
+++ lib/actions/createProduct.ts
@@ -5,9 +5,13 @@ import { prisma } from "../prismaClient/prisma";
 const createProduct = async (data: any) => {
   console.log("here is data:", data);
   const existingProduct = await prisma.product.findFirst({
-    where: { title: data.title },
+    where: {
+      title: data.title,
+    },
   });
+
   if (existingProduct) {
+    console.log("product alrady exits:", existingProduct);
     return existingProduct;
   }
   const product = await prisma.product.create({ data });
